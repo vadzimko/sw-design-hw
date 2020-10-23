e2ee2530327344f728a316c57151040560397233
v.badyaev <v.badyaev@corp.vk.com>
2020-10-23 19:25:10 +0300

    Initial commit

---
+++ src/main/java/info/vadzimko/refactoring/Main.java
@@ -0,0 +1,43 @@
+package info.vadzimko.refactoring;
+
+import info.vadzimko.refactoring.servlet.AddProductServlet;
+import info.vadzimko.refactoring.servlet.GetProductsServlet;
+import info.vadzimko.refactoring.servlet.QueryServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * @author akirakozov
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
+                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " PRICE          INT     NOT NULL)";
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        }
+
+        Server server = new Server(8081);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new AddProductServlet()), "/add-product");
+        context.addServlet(new ServletHolder(new GetProductsServlet()),"/get-products");
+        context.addServlet(new ServletHolder(new QueryServlet()),"/query");
+
+        server.start();
+        server.join();
+    }
+}
